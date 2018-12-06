9d9362d5d8502719ae4a643421cd3782f4e576e8
tien <tienoho@gmail.com>
2018-12-06 16:59:51 +0700

    nhu linnnnnnnnnn

--- Application/SQL/sql.sql
+++ Application/SQL/sql.sql
@@ -1,50 +1,63 @@
-CREATE DATABASE  IF NOT EXISTS `tintuc` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `tintuc`;
-
-CREATE TABLE `role` (
-  `roleid` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`roleid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-CREATE TABLE `user` (
-  `userid` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `fullname` varchar(300) DEFAULT NULL,
-  `createddate` timestamp NULL DEFAULT NULL,
-  `roleid` bigint(20) NOT NULL,
-  PRIMARY KEY (`userid`),
-  UNIQUE KEY `name` (`name`),
-  KEY `FK_USER_ROLE_idx` (`roleid`),
-  CONSTRAINT `FK_USER_ROLE` FOREIGN KEY (`roleid`) REFERENCES `role` (`roleid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
-
--- auto-generated definition
 create table category
 (
-  category_id   int auto_increment
+  category_id     int auto_increment
     primary key,
-  category_name longtext     null,
-  category_slug varchar(255) null,
+  category_name   longtext        null,
+  category_des    text            null,
+  category_slug   varchar(255)    null,
+  category_parent int default '0' null,
   constraint category_category_id_uindex
   unique (category_id)
 );
 
--- auto-generated definition
 create table post
 (
   post_id        int auto_increment
     primary key,
-  authorID       int         null,
-  post_date      datetime    null,
-  post_edit_date int         null,
-  post_content   longtext    null,
-  post_title     text        null,
-  post_status    varchar(10) null,
-  post_slug      text        null,
+  author_id      int          null,
+  post_date      datetime     null,
+  post_edit_date datetime     null,
+  post_title     text         null,
+  post_content   longtext     null,
+  post_summary   text         null,
+  post_status    varchar(10)  null,
+  post_slug      text         null,
+  post_img       varchar(255) null,
+  category_id    int          null,
   constraint post_post_id_uindex
   unique (post_id)
 );
 
+create table role
+(
+  roleid bigint auto_increment
+    primary key,
+  name   varchar(100) null
+);
+
+create table terms_relationships
+(
+  post_id     int             null,
+  category_id int             null,
+  `order`     int default '0' null
+)
+  comment 'bảng kết nối post với category';
+
+create table user
+(
+  userid      bigint auto_increment
+    primary key,
+  email       varchar(255) not null,
+  password    varchar(255) not null,
+  fullname    varchar(300) null,
+  createddate timestamp    null,
+  roleid      bigint       not null,
+  constraint user_email_uindex
+  unique (email),
+  constraint FK_USER_ROLE
+  foreign key (roleid) references role (roleid)
+);
+
+create index FK_USER_ROLE_idx
+  on user (roleid);
 
